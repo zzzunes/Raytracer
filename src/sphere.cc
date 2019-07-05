66c02c161e972d82f02f2f1b99cf5937eeb3bc28
Jay Taylor <jay.xyz@mailbox.org>
2019-07-05 19:31:23 -0400

    Made Sphere's ray_intersect algorithm more readable and small fixes

--- src/sphere.cc
+++ src/sphere.cc
@@ -1,21 +1,24 @@
 #include <geometry.h>
 #include <sphere.h>
 
-bool Sphere::ray_intersect(const Vec3f& ray_origin, const Vec3f& direction, float& object_distance) const {
-	Vec3f ray_origin_to_object_center = this->center - ray_origin;
-	float hypotenuse_squared = ray_origin_to_object_center * ray_origin_to_object_center;
-	float distance_line = ray_origin_to_object_center * direction;
+bool Sphere::ray_intersect(const Vec3f& ray_origin, const Vec3f& direction, float& sphere_distance) const {
+	Vec3f ray_origin_to_sphere_center = this->center - ray_origin;
+	float distance_line = ray_origin_to_sphere_center * direction;
+
+	float hypotenuse_squared = ray_origin_to_sphere_center * ray_origin_to_sphere_center;
 	float distance_squared = distance_line * distance_line;
-	float distance_from_ray_to_object = sqrtf(hypotenuse_squared - distance_squared);
+	float distance_from_ray_to_object_center = sqrtf(hypotenuse_squared - distance_squared);
 
-	if (distance_from_ray_to_object > this->radius) return false;
+	if (distance_from_ray_to_object_center > this->radius) return false;
 
-	float hit_distance_from_center = this->radius - distance_from_ray_to_object;
-	float hit_point_distance = distance_line - hit_distance_from_center;
-	object_distance = hit_point_distance;
-	float inside_point = distance_line + hit_distance_from_center;
-	if (object_distance < 0) object_distance = inside_point;
-	return object_distance > 0;
+	float distance_to_outside_of_the_sphere = this->radius - distance_from_ray_to_object_center;
+	float front_hit_point = distance_line - distance_to_outside_of_the_sphere;
+	sphere_distance = front_hit_point;
+	float exit_hit_point = distance_line + distance_to_outside_of_the_sphere;
+	bool front_hit_point_is_behind_ray = sphere_distance < 0;
+	if (front_hit_point_is_behind_ray) sphere_distance = exit_hit_point;
+	bool hit_point_is_in_front_of_ray = sphere_distance > 0;
+	return hit_point_is_in_front_of_ray;
 }
 
 Vec3f& Sphere::get_center() {
